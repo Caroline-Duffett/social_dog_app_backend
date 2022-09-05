02b1209d9b93a9db50d6adbdbd66a071b8fa1c67
CarolineDuffett <ckduffett@gmail.com>
2022-09-05 16:51:02 -0400

    inital setup

---
+++ server.js
@@ -0,0 +1,70 @@
+//___________________
+//Dependencies
+//___________________
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+
+
+
+//___________________
+//Controllers
+//___________________
+
+
+
+
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT
+
+
+
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const SOCIALDOG_1 = process.env.SOCIALDOG_1
+
+
+
+//___________________
+//Middleware
+//___________________
+app.use(express.json())
+app.use(cors())
+
+
+
+//___________________
+//Routes
+//___________________
+
+
+//redirect for heroku
+app.get('/', (req, res) => {
+  res.redirect('/locations')
+})
+
+
+
+//___________________
+//Listener
+//___________________
+app.listen(PORT, () => console.log('Listening on port: 3000'));
+
+// Connect to Mongo
+mongoose.connect(SOCIALDOG_1,  { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/chowder")
+
+// Error / success
+mongoose.connection.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+mongoose.connection.on('connected', () => console.log('mongo connected: ', SOCIALDOG_1));
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
+mongoose.connection.once('open', () => {
+  console.log('connected to mongod...');
+})
